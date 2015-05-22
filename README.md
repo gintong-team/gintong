60cfb01ec82a44f09930f4a956e90c300ed8b7e7
gintong-team <heimeixn@gmail.com>
2015-05-22 21:34:44 +0800

    first version

---
+++ README.md
@@ -0,0 +1,6 @@
+## WEBAPP模块化开发框架
+
+```bash
+npm install -g gintong
+gintong -v
+```
---
+++ version.js
@@ -0,0 +1,9 @@
+module.exports = function(){
+    var content = [
+        '',
+
+        'v' + fis.cli.info.version,
+        ''
+    ].join('\n');
+    console.log(content);
+};
